679f13f668480a117f079ba992f095d746da086d
Daniel Hueholt <hueholt.earth@gmail.com>
2025-11-10 10:07:43 -0700

    Add latitude weighting to example refine_map_figure

--- code/exploratory-data-analysis/refine_map_figure.py
+++ code/exploratory-data-analysis/refine_map_figure.py
@@ -1,3 +1,4 @@
+from icecream import ic
 import matplotlib.pyplot as plt
 import numpy as np
 import xarray as xr
@@ -7,10 +8,17 @@ import fun_plots as fp
 
 path = '/Users/danielhueholt/Data/ev228_data/gridded/'
 fn = 'era5_t2m_1997-2025.nc'
-out_path = '/Users/danielhueholt/Documents/Figures/ev228_fig/20251103/'
-out_fn = '3_era5.png'
+out_path = '/Users/danielhueholt/Documents/Figures/ev228_fig/20251104/'
+# out_fn = '3_era5.png'
 
 da = fi.import_era5(file_path=path + fn, var='t2m')
 da_timemn = da.mean(dim='valid_time')
 da_toplot = da_timemn - 273.15
-fp.map(da_toplot, out_path=out_path, out_name=out_fn)
+
+weights = np.cos(np.deg2rad(da_toplot['latitude']))
+da_wghtd = da_toplot.weighted(weights)
+da_timemn_wghtd = da_wghtd.mean(dim=['latitude', 'longitude'])
+ic(da_timemn_wghtd.data)
+da_timemn_nowghtd = da_toplot.mean(dim=['latitude', 'longitude'])
+ic(da_timemn_nowghtd.data)
+# fp.map(da_toplot, out_path=out_path, out_name=out_fn)
